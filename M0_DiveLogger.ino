23696ccdd4f74c992055c1c4e55c73d2065523a2
Doug <drmsys@yahoo.com>
2017-02-28 15:59:14 -0800

    Resolving differences

--- M0_DiveLogger.ino
+++ M0_DiveLogger.ino
@@ -147,14 +147,53 @@ void setup()
 }
 
 // Read the IMU sensor
-#include "M0_Output_Procs.h"
 
 void nineDoFProcess(void)
 {
   lastNINEDoFmillis = millis();
   digitalWrite(LED, HIGH);
-  Output_9DoF();
+  sensors_event_t accel, mag, gyro, temp; // all local, no global knowledge of 9 DoF
+  my9DOF.getEvent(&accel, &mag, &gyro, &temp); 
   digitalWrite(LED, LOW);
+<<<<<<< HEAD
+=======
+
+  // Write the data
+  String out = String(OUT_SIZE);
+
+  out = String(log_cnt++) + "\tacl\t" + String(accel.acceleration.x, 2) + "\t" + 
+                                        String(accel.acceleration.y, 2) + "\t" + 
+                                        String(accel.acceleration.z, 2) + "\t" +
+                                        String(sqrt(accel.acceleration.x * accel.acceleration.x + 
+                                                    accel.acceleration.y * accel.acceleration.y + 
+                                                    accel.acceleration.z * accel.acceleration.z), 2) + "\r\n" +
+        String(log_cnt++) + "\tgyr\t" + String(gyro.gyro.x, 2) + "\t" + 
+                                        String(gyro.gyro.y, 2) + "\t" + 
+                                        String(gyro.gyro.z, 2) + "\t" +
+                                        String(sqrt(gyro.gyro.x * gyro.gyro.x + 
+                                                    gyro.gyro.y * gyro.gyro.y + 
+                                                    gyro.gyro.z * gyro.gyro.z), 2) + "\r\n" +
+        String(log_cnt++) + "\tmag\t" + String(mag.magnetic.x, 2) + "\t" + 
+                                        String(mag.magnetic.y, 2) + "\t" + 
+                                        String(mag.magnetic.z, 2) + "\t" +
+                                        String(sqrt(mag.magnetic.x * mag.magnetic.x + 
+                                                    mag.magnetic.y * mag.magnetic.y + 
+                                                    mag.magnetic.z * mag.magnetic.z), 2);
+  if(serprt) Serial.println(out);
+}
+
+// print out the SAMD processor serial number
+void print_serial()
+{
+  long s[4], *ser;
+  ser = (long *) 0x0080A00C; // address of the processor serial number
+  int i;
+  for (i=3; i>=0; i--) s[i] = *ser++;
+
+  if(serprt) Serial.print("s ");
+  for(i=0; i<4; i++) if(serprt) Serial.print(s[i], HEX);
+  if(serprt) Serial.println();
+>>>>>>> parent of 5d34de3... Small Changes
 }
 
 void micro_clk_corr()
